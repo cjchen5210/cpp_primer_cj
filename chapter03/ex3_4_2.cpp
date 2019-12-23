bcc0b88bb72bdea3eb91feb7e877ce9bd7b1e2da
cjchen5210 <56478943+cjchen5210@users.noreply.github.com>
2019-12-23 19:23:14 +0800

    Update ex3_4_2.cpp

--- chapter03/ex3_4_2.cpp
+++ chapter03/ex3_4_2.cpp
@@ -25,3 +25,28 @@ int main()
     }
     return 0 ;
 }
+
+
+//answer
+/*
+#include <iostream>
+#include <string>
+using std::string;
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+	string str1, str2;
+	while (cin >> str1 >> str2)
+	{
+		if (str1.size() == str2.size())
+			cout << "The two strings have the same length." << endl;
+		else
+			cout << "The longer string is " << ((str1.size() > str2.size()) ? str1 : str2) << endl;
+	}
+
+	return 0;
+}
+*/
