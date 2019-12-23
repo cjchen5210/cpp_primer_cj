a030d61ebe58a7a444a83ac2e8cb156a59e36af4
cjchen5210 <56478943+cjchen5210@users.noreply.github.com>
2019-12-23 11:17:31 +0800

    Update ex3_2.cpp

--- chapter03/ex3_2.cpp
+++ chapter03/ex3_2.cpp
@@ -11,3 +11,22 @@ int main()
         cout << a << endl;
     return 0;
 }
+
+//answer
+/*
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string s ;
+    while (getline(cin,s))
+    {
+        cout << s << endl;
+        
+    }
+    return 0;
+}
+*/
