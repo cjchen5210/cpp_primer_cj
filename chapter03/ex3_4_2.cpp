dcd7859ebe960c7ac8f752baedb3f0f66e1e9901
cjchen5210 <56478943+cjchen5210@users.noreply.github.com>
2019-12-23 19:21:32 +0800

    Create ex3_4_2.cpp

---
+++ chapter03/ex3_4_2.cpp
@@ -0,0 +1,27 @@
+//比较字符长度大小
+#include <iostream>
+#include <string>
+
+using namespace std ;
+
+int main()
+
+{
+    string s1 ,s2 ;
+    cin >> s1 >> s2;
+    auto l1 = s1.size();
+    auto l2 = s2.size();
+    if ( l1 > l2 )
+    {
+        cout << s1 << endl;
+    }
+    else if ( l1< l2 )
+    {
+        cout << s2 << endl;
+    }
+    else
+    {
+        cout << "长度相等" << endl ;
+    }
+    return 0 ;
+}
