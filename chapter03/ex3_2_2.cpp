00f9daafebd023ab1d0611a4d8a59f3569e9a355
cjchen5210 <56478943+cjchen5210@users.noreply.github.com>
2019-12-23 12:38:23 +0800

    Update ex3_2_2.cpp

--- chapter03/ex3_2_2.cpp
+++ chapter03/ex3_2_2.cpp
@@ -1,1 +1,17 @@
+//读取一个词
+//getline是读取一行，cin只是读取一个词
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string a;
+    while(cin >> a)
+        cout << a << endl;
+    return 0;
+}
+
 
